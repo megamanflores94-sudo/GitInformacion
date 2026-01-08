6f23efb3a929f3a04224d8af9a4db846eefc01b9
serranonataly42-commits <serranonataly42@gmail.com>
2026-01-08 10:20:10 -0600

    Create Nataly.py
    Vacaciones

---
+++ Nataly.py
@@ -0,0 +1,11 @@
+print("Nataly Daniela, Serrano Garcia")
+
+print("mi fortaleza es saber escuchar bien a las personas y academicamete lengua y comunicacion")
+
+print("mi debilidad es no saber expresarme claramente y academicamente las matematicas")
+
+print("mis logros an sido aprender a controlar mi mal caracter  y academicamente intentar hacer las cosas sola")
+
+print("mi musica y artistas fav son: Luis Miguel y Lantin Mafia, escucho todo tipo de misica, como la salsa, el norteño, la banda, la musica en ingles y concines sin sonido,rock, junir h, serranonataly42-commits etc, amo la musica y a sus sonidos")
+
+print("Resumen: en resumen, estuve trabajando y  despues me fui a Acapulco, que el temblor estubo horrible ahi y más las replicas, en fin regresando otra vez a trabajar y a la escuelita.")
